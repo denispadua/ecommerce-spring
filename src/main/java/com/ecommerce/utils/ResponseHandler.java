a1b3f438621d844f61ae9fe21b37bb450abf4777
denispadua <38124212+denispadua@users.noreply.github.com>
2023-05-14 15:54:21 -0300

    Changed parameter name

--- src/main/java/com/ecommerce/utils/ResponseHandler.java
+++ src/main/java/com/ecommerce/utils/ResponseHandler.java
@@ -10,15 +10,15 @@ import org.springframework.http.ResponseEntity;
 
 public class ResponseHandler {
     
-    public static <T> ResponseEntity<Object> response(List<T> lsUser, HttpStatus status){
+    public static <T> ResponseEntity<Object> response(List<T> lsData, HttpStatus status){
         Map<String, Object> map = new HashMap<String, Object>();
-        map.put("data", lsUser);
+        map.put("data", lsData);
         return new ResponseEntity<Object>(map, status);
     }
 
-    public static <T> ResponseEntity<Object> response(T user, HttpStatus status){
+    public static <T> ResponseEntity<Object> response(T data, HttpStatus status){
         Map<String, Object> map = new HashMap<String, Object>();
-        map.put("data", user);
+        map.put("data", data);
         return new ResponseEntity<Object>(map, status);
     }
 }
